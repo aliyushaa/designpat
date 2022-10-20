ec97598f26940bc4e5126af535b9547054253be9
aliyushaa <97094050+aliyushaa@users.noreply.github.com>
2022-10-21 04:31:49 +0600

    yes

---
+++ pages/api/sign.js
@@ -0,0 +1,21 @@
+import connect from "../../lib/mongodb";
+import User from '../../model/schema'
+
+connect()
+//proxy patter is provides a substitute or placeholder
+//for another object. The idea is to control access to
+//the original object, performing some kind of action
+//before or after the request gets to the actual original object.
+
+
+export default async function handler(req,res){
+    try {
+        const user = await User.create(req.body);
+        res.redirect('/')
+        if(!user){
+            return res.json({"code":'User not created'})
+        }
+    } catch (error) {
+        res.status(400).json({status:'User is created!'})
+    }
+}
