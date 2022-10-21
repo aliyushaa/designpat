98430dac28e5f2b423e2a947c540ead8e0985fa2
aliyushaa <97094050+aliyushaa@users.noreply.github.com>
2022-10-21 13:37:39 +0600

    design pattern

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
 export default function Home(){
   return(
       <div>
-        <h1> Registraion </h1>
+        <h1> Registration </h1>
         <form action="/api/sign" method="post">
           <label>
             EMail Address
